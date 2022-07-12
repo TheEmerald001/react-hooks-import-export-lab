85f323e430542df3421c107cb36c1936f4af0aba
steve austin <austines811@gmail.com>
2022-07-12 16:02:32 +0300

    Done.

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,8 @@ function About() {
     <div id="about">
       <h2>About Me</h2>
       <p>I Made This</p>
-      <img src={image} alt="I made this" />
+      <img src= "https://cdn.pixabay.com/photo/2016/04/06/14/04/bird-1311822_960_720.jpg" alt="I made this" />
     </div>
   );
 }
+export default About;
